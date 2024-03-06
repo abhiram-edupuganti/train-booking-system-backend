2274a6cfa55776bf4c168a78963aaa3fe5d12256
Abhiram Edupuganti <abhiramedupuganti@Abhirams-MacBook-Air.local>
2024-03-06 13:25:09 +0530

    initial commit

---
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -0,0 +1,157 @@
+package org.example.services;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.entities.Train;
+import org.example.entities.User;
+import org.example.util.UserServiceUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+
+public class UserBookingService {
+    private User user;
+
+    private ObjectMapper objectMapper=new ObjectMapper();
+
+    private List<User> userList;
+    private static final String USERS_PATH="../localDb/users.json";
+
+    public UserBookingService(User user1) throws IOException
+    {
+        this.user=user1;
+        loadUsers();
+
+    }
+
+
+
+    public UserBookingService() throws IOException
+    {
+        loadUsers();
+    }
+
+    public List<User> loadUsers() throws IOException{
+        File users=new File(USERS_PATH);
+
+        return objectMapper.readValue(users, new TypeReference<List<User>>() {
+            @Override
+            public Type getType() {
+                return super.getType();
+            }
+        });
+    }
+
+    public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1){
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
+    }
+
+
+    private void saveUserListToFile() throws IOException {
+        File usersFile = new File(USERS_PATH);
+        objectMapper.writeValue(usersFile, userList);
+    }
+
+    public void fetchBookings(){
+        Optional<User> userFetched = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        if(userFetched.isPresent())
+        {
+            userFetched.get().printTickets();
+        }
+    }
+
+    //cancel ticket is assignment
+
+    public boolean cancelTicket(String userId)
+    {
+        Iterator<User>it= userList.iterator();
+        while(it.hasNext())
+        {
+            User u1=it.next();
+            if(u1.getUserId().equals(userId))
+            {
+                it.remove();
+                try{
+                    saveUserListToFile();
+                }
+                catch(IOException e)
+                {
+                    System.out.println(e.getMessage());
+                }
+                return Boolean.TRUE;
+            }
+        }
+
+        return Boolean.FALSE;
+    }
+
+
+    public List<Train> getTrains(String source,String destination)
+    {
+        try
+        {
+            TrainService trainService=new TrainService();
+            return trainService.searchTrains(source,destination);
+
+        }
+        catch(IOException e)
+        {
+            System.out.println(e.getMessage());
+            return new ArrayList<>();
+        }
+    }
+
+    public List<List<Integer>> fetchSeats(Train train)
+    {
+        return train.getSeats();
+    }
+
+
+    public boolean bookTrainSeat(Train train,int row,int col)
+    {
+        try{
+            TrainService trainService = new TrainService();
+            List<List<Integer>> seats = train.getSeats();
+            if (row >= 0 && row < seats.size() && col >= 0 && col < seats.get(row).size()) {
+                if (seats.get(row).get(col) == 0) {
+                    seats.get(row).set(col, 1);
+                    train.setSeats(seats);
+                    trainService.addTrain(train);
+                    return true; // Booking successful
+                } else {
+                    return false; // Seat is already booked
+                }
+            } else {
+                return false; // Invalid row or seat index
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+
+}
